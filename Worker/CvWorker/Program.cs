6f2c3d2c7eae3303b89455b831816fc2dc181611
Jeroen <jegbol01@gmail.com>
2023-03-02 17:20:31 +0100

    To clean architecture

--- Worker/CvWorker/Program.cs
+++
@@ -1,7 +0,0 @@
-using CvWorker;
-
-IHost host = Host.CreateDefaultBuilder(args)
-    .ConfigureServices(services => { services.AddHostedService<Worker>(); })
-    .Build();
-
-await host.RunAsync();
