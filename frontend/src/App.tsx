05285ab7e3b6477324c2e26a59d445b09b856554
Jeroen <jegbol01@gmail.com>
2023-03-15 13:44:46 +0100

    Frontend cleanup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect} from 'react'
+import './App.css'
 
 function App() {
+  const link: string = 'wss://triplejbpubsub.webpubsub.azure.com/client/hubs/predictions?access_token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYmYiOjE2Nzg4MDcxMzcsImV4cCI6MTY3OTA2NjMzNywiaWF0IjoxNjc4ODA3MTM3LCJhdWQiOiJodHRwczovL3RyaXBsZWpicHVic3ViLndlYnB1YnN1Yi5henVyZS5jb20vY2xpZW50L2h1YnMvcHJlZGljdGlvbnMifQ.mfaA5Uqx3V_o1QixFvfPbrrTMdmEJwGjZUhfXJtFzts'
+
+  useEffect(() => {
+    const ws = new WebSocket(link)
+
+    ws.onopen = () => {
+      console.log('Connected!')
+    }
+
+    ws.onmessage = (event) => {
+      console.log(JSON.parse(event.data))
+    }
+
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
