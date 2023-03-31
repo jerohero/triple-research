c1318219abe1045cd2586a4cf521907d95f7a8b3
Jeroen <jegbol01@gmail.com>
2023-03-31 16:43:21 +0200

    Fixed directories

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,8 @@ import React, {useEffect, useState} from 'react'
 import './App.css'
 
 function App() {
-  const link: string = '' // For now, put the outputted ws:// URL from the worker here
+  const link: string = 'wss://triplejbpubsub.webpubsub.azure.com/client/hubs/predictions?access_token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYmYiOjE2ODAyNjQ3NTYsImV4cCI6MTY4MDUyMzk1NiwiaWF0IjoxNjgwMjY0NzU2LCJhdWQiOiJodHRwczovL3RyaXBsZWpicHVic3ViLndlYnB1YnN1Yi5henVyZS5jb20vY2xpZW50L2h1YnMvcHJlZGljdGlvbnMifQ._D2zmIQRQZ4pkol\n' +
+    '-Et3OHVhYd4NMHolvKLniOgNW3Cc'
 
   const [events, setEvents]: any = useState([])
 
