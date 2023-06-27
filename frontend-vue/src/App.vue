d78f0e278ecafeb985e517423115489986b8feaa
Jeroen <jegbol01@gmail.com>
2023-06-27 11:29:07 +0200

    Removed sidebar

--- frontend-vue/src/App.vue
+++ frontend-vue/src/App.vue
@@ -1,14 +1,12 @@
 <script setup lang="ts">
   import { RouterView } from 'vue-router'
   import NavHeader from './components/NavHeader.vue'
-  import NavSidebar from '@/components/NavSidebar.vue'
 </script>
 
 <template>
   <NavHeader/>
-  <NavSidebar/>
   <div
-      class="text-text min-h-screen pl-[var(--sidebar-width)] pt-[var(--header-height)]"
+      class="text-text min-h-screen pt-[var(--header-height)]"
   >
     <RouterView/>
   </div>
